f0b6463ec57c8839f9259a4a71c1a8d33714a21c
Ratnakar Sahoo <ratnakar3895@yahoo.com>
2021-08-14 00:09:21 +0530

    chat up to work

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { ChatEngine } from 'react-chat-engine';
 import './App.css';
+import ChatFeed from './components/ChatFeed';
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             projectID="1766dee3-be6d-4d0f-b389-601cb27e2241"
             userName="Ratnakar5938"
             userSecret="ratnakar5938"
+            renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
         />
     )
 }
